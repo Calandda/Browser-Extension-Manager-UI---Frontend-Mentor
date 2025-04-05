5ac8a8532c16b487e122adf9db6b82312bc4f2dd
Calandda <A.calandda@gmail.com>
2025-04-05 22:47:26 +0800

    Added initial function for getting JSON file to JS

---
+++ data/data.js
@@ -0,0 +1,24 @@
+class dataObject{
+    constructor(){
+        this.dataJSON = {};
+        this.externalJSONExtract();
+    }
+    test(){
+        console.log('test DATA');
+    }
+    JSONCheck(){
+        console.log('JSON CHECK');
+        console.log(this.dataJSON);
+    }
+    async externalJSONExtract(){
+        fetch('../data/data.json')
+            .then((response) =>{
+                return response.json();
+            })
+            .then((response) =>{
+                this.dataJSON = response;
+            })
+    }
+}
+
+export default dataObject;
--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('test');
+import domObject from './DOM/dom.js';
+import dataObject from './data/data.js';
+
+const dataOrigin = new dataObject();
+
+dataOrigin.test();
+
+const bodyHTML = document.querySelector('body');
+
+bodyHTML.addEventListener("click",(e)=>{
+    dataOrigin.JSONCheck();
+})
