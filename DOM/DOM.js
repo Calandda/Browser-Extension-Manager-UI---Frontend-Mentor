2f61b88634fd0312fac83020a16c5647c83a7422
Calandda <A.calandda@gmail.com>
2025-04-07 22:34:04 +0800

    Added button color change function

--- DOM/DOM.js
+++ DOM/DOM.js
@@ -1,5 +1,22 @@
 class domObject{
     constructor(){
+       
+    }
+    buttonColorChange(buttonClass){
+        const buttonAll = document.querySelector('.buttonAll');
+        const buttonActive = document.querySelector('.buttonActive');
+        const buttonInactive = document.querySelector('.buttonInactive');
+        const buttonClicked = document.querySelector('.'+buttonClass);
+        let colors = ['colorBG_Neutral0', 'colorBG_Red700'];
+        
+        buttonAll.classList.remove(colors[0],colors[1]);
+        buttonActive.classList.remove(colors[0],colors[1]);
+        buttonInactive.classList.remove(colors[0],colors[1]);
+        
+        buttonAll.classList.add(colors[0]);
+        buttonActive.classList.add(colors[0]);
+        buttonInactive.classList.add(colors[0]);
+        buttonClicked.classList.add(colors[1]);
     }
 }
 
--- script.js
+++ script.js
@@ -2,11 +2,18 @@ import domObject from './DOM/DOM.js';
 import dataObject from './data/data.js';
 
 const dataOrigin = new dataObject();
+const domOrigin = new domObject();
 
 dataOrigin.test();
 
 const bodyHTML = document.querySelector('body');
 
 bodyHTML.addEventListener("click",(e)=>{
-    dataOrigin.JSONCheck();
+    let currentPress = e.target.className.split(' ')[0];
+    //console.log(e.target.tagName);
+    if(e.target.tagName === 'BUTTON'){
+        domOrigin.buttonColorChange(currentPress);
+    }
+    //dataOrigin.JSONCheck();
 })
+
